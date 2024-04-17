f7182af7882a23431c5965512614619d36795b0b
Samir <samircd9@gmail.com>
2024-04-16 21:45:29 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+import { fastify } from "fastify";
+import { DatabaseMemory } from "./database-memory.js";
+
+const server = fastify();
+const database = new DatabaseMemory();
+
+server.post('/videos', (request, reply) => {
+    const {title, description, duration} = request.body;
+    database.create({
+        title,
+        description,
+        duration,
+    })
+
+    return reply.status(201).send();
+});
+
+server.get('/videos', (request, reply) => {
+    const search = request.query.search;
+    const videos = database.list(search);
+    return reply.status(200).send(videos);
+});
+
+server.put('/videos/:id', (request, reply) => {
+    const videoId = request.params.id;
+    const video = request.body;
+    database.update(videoId, video);
+    return reply.status(204).send();
+});
+
+server.delete('/videos/:id', (request, reply) => {
+    const videoId = request.params.id;
+    database.delete(videoId);
+    return reply.status(204).send();
+});
+
+server.listen({
+    port: 3333
+});
